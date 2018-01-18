0bbe07617357409c0ff6b9a252ed35dfac40301c
Jia Lu <lujiacn@gmail.com>
2018-01-18 12:45:07 +0800

    rollback go gopkg.in/mgo

--- revmgo.go
+++ revmgo.go
@@ -6,10 +6,10 @@ import (
 	"strings"
 
 	"github.com/revel/revel"
-	//mgo "gopkg.in/mgo.v2"
-	//"gopkg.in/mgo.v2/bson"
-	"github.com/globalsign/mgo"
-	"github.com/globalsign/mgo/bson"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	//"github.com/globalsign/mgo"
+	//"github.com/globalsign/mgo/bson"
 )
 
 type MgoController struct {
